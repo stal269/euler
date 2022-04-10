603fcad8b0d4e943daad1b424b6541fd3de313a5
shimital <shimital269@gmail.com>
2022-04-10 20:46:02 +0300

    improve solution

--- haskell/app/p58.hs
+++ haskell/app/p58.hs
@@ -1,37 +1,39 @@
-{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
-{-# OPTIONS_GHC -Wno-overlapping-patterns #-}
 module Main where
 
 import Data.Numbers.Primes
-import Data.List ()
-import Data.Fixed
-import qualified Data.Set as Set
-import GHC.Base (divInt)
 import GHC.Num (divInteger)
 
-maxSide = 1000000
 threshold = 0.1
 
-sumPrefix :: [Integer] -> Int -> Integer
-sumPrefix list size = sum (1:take size list)
+idx :: Integer -> Integer
+idx i = i `div` 2 + i `mod` 2
 
-mapElemToSum :: [Integer] -> [Integer]
-mapElemToSum list = [x | i <- [1..(length list)], let x = sumPrefix list i] 
+lastElem :: Integer -> Integer -> Integer -> Integer
+lastElem first i d = first + (i - 1) * d
 
-diagonal_1 = take maxSide [1 + d | i <- [2, 4..], let d = sum [2, 4..i]]
+seqSum :: Integer -> Integer -> Integer -> Integer
+seqSum first i  d = i * (first + last) `div` 2
+    where last = lastElem first i d
 
-diagonal_2 = mapElemToSum (take maxSide (concat [[i, i] | i <- [4, 8..]]))
+diagonal_1_elem :: Integer -> Integer
+diagonal_1_elem i = 1 + seqSum 2 i 2
 
-spiralPrimesHelper :: [Integer] -> [Integer] -> Integer -> Integer -> Integer
-spiralPrimesHelper [] [] total pNum = total
-spiralPrimesHelper (x1:x2:xs) (y1:y2:ys) total pNum
+diagonal_2_elem :: Integer -> Integer
+diagonal_2_elem i = (1 + 2 * seqSum 4 norI 4) - ((i `mod` 2) * lastElem 4 norI 4)
+    where norI = idx i
+
+spiralPrimesHelper :: Integer -> Integer -> Integer -> Integer
+spiralPrimesHelper idx total pNum
     | pPercentage < threshold = (newTotal `divInteger` 2) + 1
-    | otherwise = spiralPrimesHelper xs ys newTotal newPNum
+    | otherwise = spiralPrimesHelper (idx+2) newTotal newPNum
     where
+        (x1, x2) = (diagonal_1_elem idx, diagonal_1_elem (idx+1))
+        (y1, y2) = (diagonal_2_elem idx, diagonal_2_elem (idx+1))
         newTotal = total + 4
         newPNum = pNum + toInteger (length (filter isPrime [x1,x2,y1,y2]))
         pPercentage = fromIntegral newPNum / (fromIntegral newTotal + 1)
 
 main :: IO ()
-main = print (spiralPrimesHelper diagonal_1 diagonal_2 0 0)
+main = print (spiralPrimesHelper 1 0 0)
+    
 
