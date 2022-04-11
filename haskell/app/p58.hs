c5b9f4166a065e7bf3af8a9f6e95ee70c24cc140
shimital <shimital269@gmail.com>
2022-04-11 12:40:04 +0300

    add benchmark

--- haskell/app/p58.hs
+++ haskell/app/p58.hs
@@ -1,7 +1,8 @@
 module Main where
 
-import Data.Numbers.Primes
+import Data.Numbers.Primes (isPrime)
 import GHC.Num (divInteger)
+import Criterion.Main
 
 threshold = 0.1
 
@@ -15,11 +16,11 @@ seqSum :: Integer -> Integer -> Integer -> Integer
 seqSum first i  d = i * (first + last) `div` 2
     where last = lastElem first i d
 
-diagonal_1_elem :: Integer -> Integer
-diagonal_1_elem i = 1 + seqSum 2 i 2
+d1 :: Integer -> Integer
+d1 i = 1 + seqSum 2 i 2
 
-diagonal_2_elem :: Integer -> Integer
-diagonal_2_elem i = (1 + 2 * seqSum 4 norI 4) - ((i `mod` 2) * lastElem 4 norI 4)
+d2 :: Integer -> Integer
+d2 i = (1 + 2 * seqSum 4 norI 4) - ((i `mod` 2) * lastElem 4 norI 4)
     where norI = idx i
 
 spiralPrimesHelper :: Integer -> Integer -> Integer -> Integer
@@ -27,13 +28,18 @@ spiralPrimesHelper idx total pNum
     | pPercentage < threshold = (newTotal `divInteger` 2) + 1
     | otherwise = spiralPrimesHelper (idx+2) newTotal newPNum
     where
-        (x1, x2) = (diagonal_1_elem idx, diagonal_1_elem (idx+1))
-        (y1, y2) = (diagonal_2_elem idx, diagonal_2_elem (idx+1))
+        (x1, x2) = (d1 idx, d1 (idx+1))
+        (y1, y2) = (d2 idx, d2 (idx+1))
         newTotal = total + 4
         newPNum = pNum + toInteger (length (filter isPrime [x1,x2,y1,y2]))
         pPercentage = fromIntegral newPNum / (fromIntegral newTotal + 1)
 
-main :: IO ()
-main = print (spiralPrimesHelper 1 0 0)
+
+-- main :: IO ()
+main = defaultMain [
+    bgroup "spiral" [
+                        bench "1" $ whnf (spiralPrimesHelper 1 0) 0
+                    ]
+                   ]
     
 
