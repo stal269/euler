731c71da77edf01d246de38bdede03d6ac5e61b9
shimital <shimital269@gmail.com>
2022-05-19 12:45:49 +0300

    remove redundant imports

--- haskell/app/p62.hs
+++ haskell/app/p62.hs
@@ -1,8 +1,5 @@
 module Main where
-import Math.NumberTheory.Roots
 import Data.List
-import Data.Set (Set)
-import qualified Data.Set as Set
 import Data.Map (Map)
 import qualified Data.Map as Map
 
