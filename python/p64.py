cddb306f63a3483883822721459dfc71f89421d1
shimital <shimital269@gmail.com>
2022-06-08 13:58:20 +0300

    small changes

--- python/p64.py
+++ python/p64.py
@@ -5,6 +5,9 @@ class R:
         self.n = n
         self.d = d
 
+    def val(self):
+        return self.n / self.d
+
     def __str__(self):
         return str(self.n) + " / " + str(self.d)
 
@@ -12,39 +15,26 @@ def getBaseNumber(num):
     return floor(sqrt(num))
 
 def gcd(a, b):
-    if(b == 0):
-        return a
-    else:
-        return gcd(b, a % b)
-
-def rValue(frac):
-    return frac.n / frac.d
+    return a if (b == 0) else gcd(b, a % b)
 
 def isOddPeriod(num):
     base = getBaseNumber(num)
     if (base == sqrt(num)): return False
-    periods = []
+    period = []
     dupSet = set()
     next = R(1, sqrt(num) - base)
 
-    while rValue(R(next.d, next.n)) not in dupSet:
-        dupSet.add(rValue(R(next.d, next.n)))
+    while (R(next.d, next.n).val()) not in dupSet:
+        dupSet.add(R(next.d, next.n).val())
         secondN = ((next.d - sqrt(num)) * -1)
-        nextN = next.n
         secondD = round((sqrt(num) + secondN) *  next.d)
-        myGcd = gcd(nextN, secondD)
+        myGcd = gcd(next.n, secondD)
         second = R(secondN * (next.n / myGcd), secondD / myGcd)
-        periods.append((second.n + base) // second.d)
+        period.append((second.n + base) // second.d)
         remainder = (second.n + base) % second.d
         third = R(sqrt(num) - (base - remainder), second.d)
         next = R(third.d, third.n)
-    return len(periods) % 2 == 1
+    return len(period) % 2 == 1
 
-count = 0
-
-for i in range(2,10001):
-    if isOddPeriod(i):
-        count += 1
-
-print (count)
+print (len([i for i in range(2, 10001) if isOddPeriod(i)]))
 
